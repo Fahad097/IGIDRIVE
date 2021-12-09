00b1535df9deaf660821033e3750049008b5ca8b
Fahad097 <68201500+Fahad097@users.noreply.github.com>
2021-12-09 15:01:07 +0500

    Add files via upload

---
+++ lib/helpers/DataSaveArray.dart
@@ -0,0 +1,37 @@
+double overallRating ;
+double speedRating ;
+double breakRating ;
+double accelerationRating ;
+double corneringRating ;
+
+bool TripLoadedCheck = false;
+bool EventLoadedCheck = false;
+bool ratingLoadCheck = false;
+
+
+
+class eventList {
+  static var eventListData = [];
+}
+
+class NotificationListt {
+  static var NotificationListData = [];
+}
+
+
+
+
+class tripList {
+  static var tripListData = [];
+}
+
+class triptransList {
+  static var triptransListData = [];
+}
+class NottList {
+  static var notListData = [];
+}
+
+
+String usernameGlobal = "";
+String usernameEmail = "";
